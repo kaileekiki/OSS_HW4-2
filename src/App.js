3d48f865400ba33a14baf12a758ae322e20220ee
kai <lucas0606@handong.ac.kr>
2024-10-05 18:27:15 +0900

    yayyy

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
   return (
     <div>
       <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <a className="navbar-brand">
+        <p3>
           22300593 이지광
-        </a>
+        </p3>
         <div className="navbar-nav mr-auto">
           <li className="nav-item">
             <Link to={"/people"} className="nav-link">
